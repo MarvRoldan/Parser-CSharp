6e51ae013808252710888ac8622eba62d843309c
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-09 16:41:40 -0600

    Added do while loop for options

--- Program.cs
+++ Program.cs
@@ -4,14 +4,22 @@ namespace C__Parser
 {
     class Program
     {
-        static void menuOptions()
+        static void userChoice()
         {
-            Console.WriteLine("Please Enter: 1, 2, or 3");
+            string userchoice;
+
+            do
+            {
+                Console.WriteLine("Enter: 1 to access file. 2 to edit file. 3 to end program");
+                userchoice = Console.ReadLine();
+            } while (userchoice != "1" && userchoice != "2" && userchoice != "3");
+
         }
         static void Main(string[] args)
         {
+            userChoice();
 
-            menuOptions();
+
         }
     }
 }

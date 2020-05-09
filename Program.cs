8f392f268e37aa8dc6438aff737db058494f4971
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-09 16:21:13 -0600

    Initial commit. Added menu options

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C__Parser
+{
+    class Program
+    {
+        static void menuOptions()
+        {
+            Console.WriteLine("Please Enter: 1, 2, or 3");
+        }
+        static void Main(string[] args)
+        {
+
+            menuOptions();
+        }
+    }
+}

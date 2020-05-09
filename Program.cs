031e28bd448f6bff06c487ed5f0c788e525fca60
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-09 17:02:31 -0600

    Added userfile

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
 
 namespace C__Parser
 {
@@ -8,10 +10,11 @@ namespace C__Parser
         static void Main(string[] args)
         {
             string userchoice;
+            string userFile = "userfile.txt";
 
             do
             {
-                Console.WriteLine("Enter: 1 to access file. 2 to edit file. 3 to end program.");
+                Console.WriteLine("Enter: 1 to access file. 2 to view file. 3 to end program.");
                 userchoice = Console.ReadLine();
             } while (userchoice != "1" && userchoice != "2" && userchoice != "3");
 

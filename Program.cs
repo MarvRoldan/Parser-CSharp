c8a5ee7d34b2ab280979911f19da0613cbe7babd
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-09 16:55:35 -0600

    Starting if for option 1

--- Program.cs
+++ Program.cs
@@ -4,22 +4,22 @@ namespace C__Parser
 {
     class Program
     {
-        static void userChoice()
+
+        static void Main(string[] args)
         {
             string userchoice;
 
             do
             {
-                Console.WriteLine("Enter: 1 to access file. 2 to edit file. 3 to end program");
+                Console.WriteLine("Enter: 1 to access file. 2 to edit file. 3 to end program.");
                 userchoice = Console.ReadLine();
             } while (userchoice != "1" && userchoice != "2" && userchoice != "3");
 
-        }
-        static void Main(string[] args)
-        {
-            userChoice();
-
-
+            if (userchoice == "1")
+            {   
+                Console.WriteLine("Enter file name");
+                Console.ReadLine();
+            }
         }
     }
 }
